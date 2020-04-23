05b22fcef0703518725262539b655e52335b6b26
Angel Torres <avto9711@gmail.com>
2020-04-23 15:50:09 -0400

    working without scraper

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const excelP = require('./src/excelProcess')
 
 const express = require('express')
 const app = express()
+const  env  = require("dotenv");
 
+env.config()
+
+const urlExcel = process.env.URL_EXCEL
 
 const USDapiUrl = "/api/v1/USD";
 app.get('/', async function (req, res) {
@@ -10,7 +14,7 @@ app.get('/', async function (req, res) {
     res.send(`${USDapiUrl} for detatils`);
 })
 app.get(USDapiUrl, async function (req, res) {
-    let USDObj = await excelP("https://cdn.bancentral.gov.do/documents/estadisticas/mercado-cambiario/documents/TASA_DOLAR_REFERENCIA_MC.xls");
+    let USDObj = await excelP(urlExcel);
     res.send(USDObj)
 })
 const port  = process.env.PORT || 1337
